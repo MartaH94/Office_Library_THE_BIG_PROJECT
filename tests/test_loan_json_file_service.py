61bc0438827b685dc7aff60c4d8b34a1146b5bdb
Marta Haraj <marta.haraj@accenture.com>
2026-06-22 12:18:38 +0200

    feat: implement unit tests for deleting reservation data in LoanJsonFileService

--- tests/test_loan_json_file_service.py
+++ tests/test_loan_json_file_service.py
@@ -8,11 +8,9 @@ ________________________________________________________
 Test classes: 9
 Test cases total: 35
 
-current status: Done --> During Update
-Total number of done test cases: 21 -->
+current status: Done
+Total number of done test cases: 35
 
-FILE UPDATE
-Needed implementation of unit test for reservation methods
 
 """
 
@@ -715,29 +713,78 @@ class TestLoanJsonFileServiceGetAllReservationList(unittest.TestCase):  # 3/3
         self.assertIn("No reservation found in the database", str(cm.exception))
 
 
-class TestLoanJsonFileServiceDeleteReservationData(unittest.TestCase):  # 0/3
+class TestLoanJsonFileServiceDeleteReservationData(unittest.TestCase):  # 3/3
     """Method under the test: delete_reservation_data
     Number of TestCases: 3
-    Done TestCases:
+    Done TestCases: 3
     """
 
     def setUp(self):
         self.temporary_dir = tempfile.TemporaryDirectory()
+        self.temporary_dir_path = Path(self.temporary_dir.name)
+        self.test_json_file_path = self.temporary_dir_path / "test_file.json"
+
+        self.valid_reservation_list = [
+            {
+                "reservation_id": 12345,
+                "user_id": 11223344,
+                "book_id": 123789987,
+                "reservation_date": "2026-06-22",
+            },
+            {
+                "reservation_id": 12456,
+                "user_id": 147258369,
+                "book_id": 258369147,
+                "reservation_date": "2026-06-01",
+            },
+        ]
+
+        with self.test_json_file_path.open("w", encoding="utf-8") as f:
+            json.dump(self.valid_reservation_list, f)
+
+        self.major_json_service = JsonFilesService(
+            file_path=self.test_json_file_path, schema=reservation_schema
+        )
+
+        self.reservation_service = LoanJsonFileService(
+            self.major_json_service, file_path=self.test_json_file_path
+        )
 
     def tearDown(self):
         self.temporary_dir.cleanup()
 
     def test_raises_validation_error_when_resrvation_id_is_none(self):
-        """expected behavior:"""
-        pass
+        """expected behavior: raises ValidationError when reservation_id is None"""
+
+        with self.assertRaises(exc.ValidationError) as cm:
+            self.reservation_service.delete_reservation_data(reservation_id=None)
+
+        self.assertIn(
+            "Reservation ID is missing or it's an empty value", str(cm.exception)
+        )
 
     def test_raises_reservation_not_found_error_when_reservation_id_not_found(self):
-        """expected behavior:"""
-        pass
+        """expected behavior: raises ReservationNotFoundError when reservationId does not exist in database"""
+
+        with self.assertRaises(exc.ReservationNotFoundError) as cm:
+            self.reservation_service.delete_reservation_data(23456)
+
+        self.assertIn("could not be removed from the database", str(cm.exception))
 
     def test_removes_reservation_and_writes_json_when_reservation_id_exists(self):
-        """expected behavior:"""
-        pass
+        """expected behavior: removes reservation entry from database and writes updated data to json file when reservation_id exists in database"""
+
+        test_result = self.reservation_service.delete_reservation_data(12345)
+
+        self.assertIn("has been removed from database", test_result)
+
+        with self.test_json_file_path.open("r", encoding="utf-8") as f:
+            updated_data = json.load(f)
+
+        self.assertEqual(len(updated_data), 1)
+
+        for reservation in updated_data:
+            self.assertNotEqual(reservation.get("reservation_id"), 12345)
 
 
 if __name__ == "__main__":
